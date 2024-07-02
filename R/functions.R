5cc3e774f32c3c31471178d4d885d7d69c8c72c4
James Steele <james.steele@solent.ac.uk>
2024-07-02 11:49:02 +0100

    targets - data prepared

---
+++ R/functions.R
@@ -0,0 +1,399 @@
+##### Function to prepare data
+prepare_data <- function(file) {
+  dat <- read_csv(file) |>
+    janitor::clean_names() |>
+    pivot_longer(5:22,
+                 names_to = "method",
+                 values_to = "jump_height") |>
+    separate(method, c("method","jump", "trial", "session")) |>
+    select(-jump) |>
+    filter(method != "FDFT") |>
+    mutate(trial = as.numeric(trial)) |>
+    mutate(session = as.numeric(
+      case_when(
+        session == "WK1" ~ 1,
+        session == "WK2" ~ 2
+      )
+    ))
+}
+
+##### Functions to fit agreement and reliability models
+fit_agree_models <- function(data) {
+  
+  agree_models <- tibble(method = as.character(),
+                         mean_bias = as.numeric(),
+                         lower_ci_mean = as.numeric(), 
+                         upper_ci_mean = as.numeric(), 
+                         lower_ci_l_loa = as.numeric(), 
+                         upper_ci_l_loa = as.numeric(),
+                         lower_ci_u_loa = as.numeric(),
+                         upper_ci_u_loa = as.numeric(),
+                         l_loa = as.numeric(),
+                         u_loa = as.numeric())
+  
+  set.seed(1988)
+  
+  for(i in c("jmc", "mj", "opto")) {
+    
+    data_wide <- data |>
+      filter(method == "fdi" | method == i) |>
+      mutate(method = case_when(
+        method == "fdi" ~ "M1",
+        method == "jmc" ~ "M2",
+        method == "mj" ~ "M2",
+        method == "opto" ~ "M2"
+      )) |>
+      pivot_wider(id_cols = c(ID_num,session_no,trial),
+                  names_from = "method",
+                  values_from = "jump_height") |>
+      mutate(
+        mean = (M2 + M1)/2,
+        diff = M2 - M1
+      )
+    
+    lmer_diff <- lmer(diff ~ (1|ID_num/session_no),
+                      data = data_wide,
+                      REML = TRUE)
+    
+    totalsd <- sqrt(as.numeric(summary(lmer_diff)$varcor[1])+
+                      as.numeric(summary(lmer_diff)$varcor[2])+
+                      as.numeric(summary(lmer_diff)$sigma^2)
+    )
+    
+    boot_lmer_diff <- case_bootstrap(lmer_diff, .f = extract_parameters, resample = c(TRUE,FALSE,FALSE), B = 10000)
+    
+    summary_boot_lmer_diff <- boot_lmer_diff$replicates |>
+      mutate(
+        totalsd = sqrt(vc1 + vc2 + vc3),
+        l_loa = beta - qnorm(1-0.05/2)*totalsd,
+        u_loa = beta + qnorm(1-0.05/2)*totalsd
+      ) |>
+      summarise(
+        mean_bias = fixef(lmer_diff)[[1]],
+        lower_ci_mean = quantile(beta, 0.025),
+        upper_ci_mean = quantile(beta, 0.975),
+        lower_ci_l_loa = quantile(l_loa, 0.025),
+        upper_ci_l_loa = quantile(l_loa, 0.975),
+        lower_ci_u_loa = quantile(u_loa, 0.025),
+        upper_ci_u_loa = quantile(u_loa, 0.975),
+        l_loa = mean_bias - sqrt(as.numeric(summary(lmer_diff)$varcor[1])+
+                                   as.numeric(summary(lmer_diff)$varcor[2])+
+                                   as.numeric(summary(lmer_diff)$sigma^2)) * qnorm(1-0.05/2),
+        u_loa = mean_bias + sqrt(as.numeric(summary(lmer_diff)$varcor[1])+
+                                   as.numeric(summary(lmer_diff)$varcor[2])+
+                                   as.numeric(summary(lmer_diff)$sigma^2)) * qnorm(1-0.05/2)
+      )
+    
+    agree_models <- bind_rows(agree_models,
+                              tibble(method = i,
+                                     summary_boot_lmer_diff))
+    
+  }
+  
+  return(agree_models)
+  
+}
+
+
+fit_reli_models <- function(data) {
+  reli_models <- tibble(method = as.character(),
+                        mean_bias = as.numeric(),
+                        lower_ci_mean = as.numeric(), 
+                        upper_ci_mean = as.numeric(), 
+                        lower_ci_l_loa = as.numeric(), 
+                        upper_ci_l_loa = as.numeric(),
+                        lower_ci_u_loa = as.numeric(),
+                        upper_ci_u_loa = as.numeric(),
+                        l_loa = as.numeric(),
+                        u_loa = as.numeric())
+  
+  set.seed(1988)
+  
+  for(i in c("fdi", "jmc", "mj", "opto")) {
+    
+    data_wide <- data |>
+      filter(method == i) |>
+      pivot_wider(id_cols = c(ID_num,trial),
+                  names_from = "session_no",
+                  values_from = "jump_height") |>
+      mutate(
+        mean = (`2` + `1`)/2,
+        diff = `2` - `1`
+      )
+    
+    lmer_diff <- lmer(diff ~ (1|ID_num),
+                      data = data_wide,
+                      REML = TRUE)
+    
+    totalsd <- sqrt(as.numeric(summary(lmer_diff)$varcor[1])+
+                      as.numeric(summary(lmer_diff)$sigma^2)
+    )
+    
+    boot_lmer_diff <- case_bootstrap(lmer_diff, .f = extract_parameters, resample = c(TRUE,FALSE), B = 10000)
+    
+    summary_boot_lmer_diff <- boot_lmer_diff$replicates |>
+      mutate(
+        totalsd = sqrt(vc1 + vc2),
+        l_loa = beta - qnorm(1-0.05/2)*totalsd,
+        u_loa = beta + qnorm(1-0.05/2)*totalsd
+      ) |>
+      summarise(
+        mean_bias = fixef(lmer_diff)[[1]],
+        lower_ci_mean = quantile(beta, 0.025),
+        upper_ci_mean = quantile(beta, 0.975),
+        lower_ci_l_loa = quantile(l_loa, 0.025),
+        upper_ci_l_loa = quantile(l_loa, 0.975),
+        lower_ci_u_loa = quantile(u_loa, 0.025),
+        upper_ci_u_loa = quantile(u_loa, 0.975),
+        l_loa = mean_bias - sqrt(as.numeric(summary(lmer_diff)$varcor[1])+
+                                   as.numeric(summary(lmer_diff)$sigma^2)) * qnorm(1-0.05/2),
+        u_loa = mean_bias + sqrt(as.numeric(summary(lmer_diff)$varcor[1])+
+                                   as.numeric(summary(lmer_diff)$sigma^2)) * qnorm(1-0.05/2)
+      )
+    
+    reli_models <- bind_rows(reli_models,
+                             tibble(method = i,
+                                    summary_boot_lmer_diff))
+    
+  }
+  
+  return(reli_models)
+}
+
+##### Funtions for creating results plots
+
+make_agree_plot <- function(data, agree_models) {
+  
+  plots <- list()
+  
+  for(i in c("jmc", "mj", "opto")) {
+    
+    data_wide <- data |>
+      filter(method == "fdi" | method == i) |>
+      mutate(method = case_when(
+        method == "fdi" ~ "M1",
+        method == "jmc" ~ "M2",
+        method == "mj" ~ "M2",
+        method == "opto" ~ "M2"
+      )) |>
+      pivot_wider(id_cols = c(ID_num,session_no,trial),
+                  names_from = "method",
+                  values_from = "jump_height") |>
+      mutate(
+        mean = (M2 + M1)/2,
+        diff = M2 - M1
+      )
+    
+    agree_model <- agree_models |>
+      filter(method == i)
+    
+    plot <- data_wide |>
+      ggplot(aes(x = mean, y = diff)) +
+      
+      # Add reference line at zero
+      geom_hline(yintercept = 0, linetype = "dashed") +
+      
+      # Add raw data
+      geom_point(alpha = 0.75) +
+      
+      # Add mean bias
+      annotate("rect", alpha = 0.25,
+               xmin = -Inf, xmax = Inf,
+               ymin = agree_model$lower_ci_mean,
+               ymax = agree_model$upper_ci_mean) +
+      geom_hline(yintercept = agree_model$mean_bias,
+                 size = 1) +
+      geom_text(label = glue::glue("Bias = {round(agree_model$mean_bias,2)} cm"),
+                x = max(data_wide$mean) + 0.75,
+                y = agree_model$upper_ci_mean + 1,
+                size = 3,
+                # parse = TRUE
+      ) +
+      geom_text(label = glue::glue("[95%CI: {round(agree_model$lower_ci_mean,2)}, {round(agree_model$upper_ci_mean,2)}]"),
+                x = max(data_wide$mean) + 0.75,
+                y = agree_model$upper_ci_mean + 0.5,
+                size = 3,
+                # parse = TRUE
+      ) +
+      
+      # Add lower LoA
+      annotate("rect", alpha = 0.25,
+               xmin = -Inf, xmax = Inf,
+               ymin = agree_model$lower_ci_l_loa[[1]],
+               ymax = agree_model$upper_ci_l_loa[[1]]) +
+      geom_hline(yintercept = agree_model$l_loa,
+                 size = 1, linetype = "dotted")  +
+      geom_text(label = glue::glue("Lower LoA = {round(agree_model$l_loa,2)} cm"),
+                x = max(data_wide$mean) + 0.75,
+                y = agree_model$lower_ci_l_loa - 1,
+                size = 3,
+                # parse = TRUE
+      ) +
+      geom_text(label = glue::glue("[95%CI: {round(agree_model$lower_ci_l_loa,2)}, {round(agree_model$upper_ci_l_loa,2)}]"),
+                x = max(data_wide$mean) + 0.75,
+                y = agree_model$lower_ci_l_loa - 1.5,
+                size = 3,
+                # parse = TRUE
+      ) +
+      
+      # Add upper LoA
+      annotate("rect", alpha = 0.25,
+               xmin = -Inf, xmax = Inf,
+               ymin = agree_model$lower_ci_u_loa,
+               ymax = agree_model$upper_ci_u_loa) +
+      geom_hline(yintercept = agree_model$u_loa,
+                 size = 1, linetype = "dotted") +
+      geom_text(label = glue::glue("Upper LoA = {round(agree_model$u_loa,2)} cm"),
+                x = max(data_wide$mean) + 0.75,
+                y = agree_model$upper_ci_u_loa + 1,
+                size = 3,
+                # parse = TRUE
+      ) +
+      geom_text(label = glue::glue("[95%CI: {round(agree_model$lower_ci_u_loa,2)}, {round(agree_model$upper_ci_u_loa,2)}]"),
+                x = max(data_wide$mean) + 0.75,
+                y = agree_model$upper_ci_u_loa + 0.5,
+                size = 3,
+                # parse = TRUE
+      ) +
+      
+      scale_x_continuous(limits = c(min(data_wide$mean), max(data_wide$mean) + 5)) +
+      scale_y_continuous(limits = c(min(data_wide$diff)-1, max(data_wide$diff) + 1)) +
+      labs(
+        x = "Mean of Measurements (cm)",
+        y = "Difference of Measurements (cm)"
+      ) +
+      theme_bw()
+    
+    
+    plots[[i]] <- plot
+    
+  }
+  
+  (plots$jmc + labs(title = "Jump Mat")) +
+    (plots$mj + labs(title = "MyJump App")) +
+    (plots$opto + labs(title = "Optojump")) +
+        plot_annotation(title = "Agreement Between Methods",
+                    subtitle = "Mixed Effects Model Mean Bias and 95% Limits of Agreement",
+                    caption = "Each method compared to gold standard (Force Decks Impulse-Momentum)\n
+                    Intervals estimates for mean bias and limits of agreement are 95% quantiles from nonparametric bootstrap resampling participants 10000 times")
+  
+}
+
+make_reli_plot <- function(data, reli_models) {
+  plots <- list()
+  
+  for(i in c("fdi", "jmc", "mj", "opto")) {
+    
+    data_wide <- data |>
+      filter(method == i)|>
+      pivot_wider(id_cols = c(ID_num,trial),
+                  names_from = "session_no",
+                  values_from = "jump_height") |>
+      mutate(
+        mean = (`2` + `1`)/2,
+        diff = `2` - `1`
+      )
+    
+    reli_model <- reli_models |>
+      filter(method == i)
+    
+    plot <- data_wide |>
+      ggplot(aes(x = mean, y = diff)) +
+      
+      # Add reference line at zero
+      geom_hline(yintercept = 0, linetype = "dashed") +
+      
+      # Add raw data
+      geom_point(alpha = 0.75) +
+      
+      # Add mean bias
+      annotate("rect", alpha = 0.25,
+               xmin = -Inf, xmax = Inf,
+               ymin = reli_model$lower_ci_mean,
+               ymax = reli_model$upper_ci_mean) +
+      geom_hline(yintercept = reli_model$mean_bias,
+                 size = 1) +
+      geom_text(label = glue::glue("Bias = {round(reli_model$mean_bias,2)} cm"),
+                x = max(data_wide$mean) + 0.75,
+                y = reli_model$upper_ci_mean + 1,
+                size = 3,
+                # parse = TRUE
+      ) +
+      geom_text(label = glue::glue("[95%CI: {round(reli_model$lower_ci_mean,2)}, {round(reli_model$upper_ci_mean,2)}]"),
+                x = max(data_wide$mean) + 0.75,
+                y = reli_model$upper_ci_mean + 0.5,
+                size = 3,
+                # parse = TRUE
+      ) +
+      
+      # Add lower LoA
+      annotate("rect", alpha = 0.25,
+               xmin = -Inf, xmax = Inf,
+               ymin = reli_model$lower_ci_l_loa[[1]],
+               ymax = reli_model$upper_ci_l_loa[[1]]) +
+      geom_hline(yintercept = reli_model$l_loa,
+                 size = 1, linetype = "dotted")  +
+      geom_text(label = glue::glue("Lower LoA = {round(reli_model$l_loa,2)} cm"),
+                x = max(data_wide$mean) + 0.75,
+                y = reli_model$lower_ci_l_loa - 1,
+                size = 3,
+                # parse = TRUE
+      ) +
+      geom_text(label = glue::glue("[95%CI: {round(reli_model$lower_ci_l_loa,2)}, {round(reli_model$upper_ci_l_loa,2)}]"),
+                x = max(data_wide$mean) + 0.75,
+                y = reli_model$lower_ci_l_loa - 1.5,
+                size = 3,
+                # parse = TRUE
+      ) +
+      
+      # Add upper LoA
+      annotate("rect", alpha = 0.25,
+               xmin = -Inf, xmax = Inf,
+               ymin = reli_model$lower_ci_u_loa,
+               ymax = reli_model$upper_ci_u_loa) +
+      geom_hline(yintercept = reli_model$u_loa,
+                 size = 1, linetype = "dotted") +
+      geom_text(label = glue::glue("Upper LoA = {round(reli_model$u_loa,2)} cm"),
+                x = max(data_wide$mean) + 0.75,
+                y = reli_model$upper_ci_u_loa + 1,
+                size = 3,
+                # parse = TRUE
+      ) +
+      geom_text(label = glue::glue("[95%CI: {round(reli_model$lower_ci_u_loa,2)}, {round(reli_model$upper_ci_u_loa,2)}]"),
+                x = max(data_wide$mean) + 0.75,
+                y = reli_model$upper_ci_u_loa + 0.5,
+                size = 3,
+                # parse = TRUE
+      ) +
+      
+      scale_x_continuous(limits = c(min(data_wide$mean), max(data_wide$mean) + 5)) +
+      scale_y_continuous(limits = c(min(data_wide$diff)-2, max(data_wide$diff) + 2)) +
+      labs(
+        x = "Mean of Measurements (cm)",
+        y = "Difference of Measurements (cm)"
+      ) +
+      theme_bw()
+    
+    
+    plots[[i]] <- plot
+    
+  }
+  
+  (plots$fdi + labs(title = "Force Decks")) +
+    (plots$jmc + labs(title = "Jump Mat")) +
+    (plots$mj + labs(title = "MyJump App")) +
+    (plots$opto + labs(title = "Optojump")) + 
+    plot_annotation(title = "Test-Retest Reliability Across Methods",
+                    subtitle = "Mixed Effects Model Mean Bias and 95% Limits of Agreement",
+                    caption = "Each method compared to gold standard (Force Decks Impulse-Momentum)\n
+                    Intervals estimates for mean bias and limits of agreement are 95% quantiles from nonparametric bootstrap resampling participants 10000 times")
+  
+  
+  
+} 
+
+make_plot_tiff <- function(plot, path, width, height, device, dpi) {
+  
+  ggsave(filename = path, plot = plot, width = width, height = height, device = device, dpi = dpi)
+  
+}
--- _targets.R
+++ _targets.R
@@ -1,62 +1,31 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
-
-# Load packages required to define the pipeline:
+# _targets.R file
 library(targets)
-# library(tarchetypes) # Load other packages as needed.
+library(tarchetypes)
+source("R/functions.R")
+tar_option_set(packages = c("tidyverse", "lme4", "lmeresampler",
+                            # "quarto", "kableExtra", 
+                            "patchwork"))
 
-# Set target options:
-tar_option_set(
-  packages = c("tibble") # Packages that your targets need for their tasks.
-  # format = "qs", # Optionally set the default storage format. qs is fast.
-  #
-  # Pipelines that take a long time to run may benefit from
-  # optional distributed computing. To use this capability
-  # in tar_make(), supply a {crew} controller
-  # as discussed at https://books.ropensci.org/targets/crew.html.
-  # Choose a controller that suits your needs. For example, the following
-  # sets a controller that scales up to a maximum of two workers
-  # which run as local R processes. Each worker launches when there is work
-  # to do and exits if 60 seconds pass with no tasks to run.
-  #
-  #   controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
-  #
-  # Alternatively, if you want workers to run on a high-performance computing
-  # cluster, select a controller from the {crew.cluster} package.
-  # For the cloud, see plugin packages like {crew.aws.batch}.
-  # The following example is a controller for Sun Grid Engine (SGE).
-  # 
-  #   controller = crew.cluster::crew_controller_sge(
-  #     # Number of workers that the pipeline can scale up to:
-  #     workers = 10,
-  #     # It is recommended to set an idle time so workers can shut themselves
-  #     # down if they are not running tasks.
-  #     seconds_idle = 120,
-  #     # Many clusters install R as an environment module, and you can load it
-  #     # with the script_lines argument. To select a specific verison of R,
-  #     # you may need to include a version string, e.g. "module load R/4.3.2".
-  #     # Check with your system administrator if you are unsure.
-  #     script_lines = "module load R"
-  #   )
-  #
-  # Set other options as needed.
-)
-
-# Run the R scripts in the R/ folder with your custom functions:
-tar_source()
-# tar_source("other_functions.R") # Source other scripts as needed.
-
-# Replace the target list below with your own:
 list(
-  tar_target(
-    name = data,
-    command = tibble(x = rnorm(100), y = rnorm(100))
-    # format = "qs" # Efficient storage for general data objects.
-  ),
-  tar_target(
-    name = model,
-    command = coefficients(lm(y ~ x, data = data))
-  )
+  # Load in data
+  tar_target(file, "data/AI_JUMP_DATA.csv", format = "file"),
+  # tar_target(file_2, "data/data_collection_s2.csv", format = "file"),
+  tar_target(data, prepare_data(file))
+  
+  # Fit agreement and reliability models
+  # tar_target(agree_models, fit_agree_models(data)),
+  # tar_target(reli_models, fit_reli_models(data)),
+  
+  # Plots
+  # tar_target(agree_plot, make_agree_plot(data, agree_models)),
+  # tar_target(reli_plot, make_reli_plot(data, reli_models)),
+  # tar_target(agree_plot_tiff, make_plot_tiff(
+  #   agree_plot, "plots/agree_plot.tiff", 12.5, 7.5, "tiff", 300
+  # )),
+  # tar_target(reli_plot_tiff, make_plot_tiff(
+  #   reli_plot, "plots/reli_plot.tiff", 10, 10, "tiff", 300
+  # )),
+  
+  # Analysis and Results write up
+  # tar_quarto(report, path = "report.qmd")
 )
